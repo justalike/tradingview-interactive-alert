05d6c8dbedb0217a026dcd7e35e1d460732be74f
Justalike <justalike.ds@gmail.com>
2024-03-19 20:55:38 +0300

    import config refactor, added websocket, preload, refactor creating series

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 
+import * as cfg from './config'
 import {createSeries, setChartSize} from './utils/utils.js';
-import { breakTrendLineSeriesConfig, trendLineSeriesConfig, rangesSeriesConfig, waveSeriesConfig, candleSeriesConfig, lineSeriesConfig, volumeSeriesConfig} from './config/seriesConfig.js';
-import { chartProperties, myPriceFormatter} from './config/chartConfig.js';
+
 import { initializeChartWithData } from './chart/chartUpdateService.js';
 import { handleCandleDataUpload } from './local/localHandler.js';
+import { preLoadHistoryCandles } from './api/dataService.js';
+import { connectWebSocket } from './api/ws.js';
 
 console.log(`_..--.._`.repeat(10))
 
@@ -17,20 +19,39 @@ chart.applyOptions({
   },
 });
 
-// Creating series
-const candleSeries = createSeries(chart, 'candlestick', candleSeriesConfig);
-const volumeSeries = createSeries(chart, 'histogram', volumeSeriesConfig);
-const lineSeries =   createSeries(chart, 'line', lineSeriesConfig);
-const waveSeries =   createSeries(chart, 'line', waveSeriesConfig);
-const trendSeries =  createSeries(chart, 'line', trendLineSeriesConfig);
-const breakTrendSeries =  createSeries(chart, 'line', breakTrendLineSeriesConfig);
-const rangesSeries =  createSeries(chart, 'line', rangesSeriesConfig);
+// // Creating series
+// const candleSeries = createSeries(chart, 'candlestick', cfg.candleSeriesConfig);
+// const volumeSeries = createSeries(chart, 'histogram', cfg.volumeSeriesConfig);
+// const lineSeries =   createSeries(chart, 'line', cfg.lineSeriesConfig);
+// const waveSeries =   createSeries(chart, 'line', cfg.waveSeriesConfig);
+// const trendSeries =  createSeries(chart, 'line', cfg.trendLineSeriesConfig);
+// const breakTrendSeries =  createSeries(chart, 'line', cfg.breakTrendLineSeriesConfig);
+// const rangesSeries =  createSeries(chart, 'line', cfg.rangesSeriesConfig);
 
-const series = { candles_series: candleSeries, volume_series: volumeSeries, extrema_series: lineSeries, wave_series: waveSeries, trend_series: trendSeries, breaktrend_series: breakTrendSeries, ranges_series: rangesSeries};
+// const series = { candles_series: candleSeries, volume_series: volumeSeries, extrema_series: lineSeries, wave_series: waveSeries, trend_series: trendSeries, breaktrend_series: breakTrendSeries, ranges_series: rangesSeries};
+
+
+const seriesTypesAndConfigs = [
+    { key: 'candles_series', type: 'candlestick', config: cfg.candleSeriesConfig },
+    { key: 'volume_series', type: 'histogram', config: cfg.volumeSeriesConfig },
+    { key: 'extrema_series', type: 'line', config: cfg.lineSeriesConfig },
+    { key: 'wave_series', type: 'line', config: cfg.waveSeriesConfig },
+    { key: 'trend_series', type: 'line', config: cfg.trendLineSeriesConfig },
+    { key: 'breaktrend_series', type: 'line', config: cfg.breakTrendLineSeriesConfig },
+    { key: 'ranges_series', type: 'line', config: cfg.rangesSeriesConfig },
+];
+
+const series = seriesTypesAndConfigs.reduce((acc, { key, type, config }) => {
+    acc[key] = createSeries(chart, type, config);
+    return acc;
+}, {});
+
 
 window.addEventListener('resize', setChartSize(chart));
+
 document.addEventListener('DOMContentLoaded', initializeChartWithData(chart, series));
 document.addEventListener('DOMContentLoaded',  connectWebSocket());
+document.addEventListener('DOMContentLoaded', preLoadHistoryCandles(symbol, timeframe))
 
 document.getElementById('dataFile').addEventListener('change', (event) => {
     const file = event.target.files[0];
